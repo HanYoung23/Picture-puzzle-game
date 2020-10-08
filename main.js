a32ccc9622970d3ac03e01a2ed3a000d78e7f208
HanYoung23 <67942048+HanYoung23@users.noreply.github.com>
2020-10-08 22:24:30 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,27 +2,32 @@ const threeBtn = document.querySelector(".threeBythree");
 const playBtn = document.querySelector(".playBtn");
 const shuffleBtn = document.querySelector(".shuffleBtn");
 const photoChangeBtn = document.querySelector(".photoChangeBtn");
+const undoBtn = document.querySelector(".undoBtn");
+const replayBtn = document.querySelector(".popUpReplayBtn");
 
 const timer = document.querySelector(".timer");
 const movement = document.querySelector(".movement");
-const originalPhoto = document.querySelector(".originalPhoto");
+const ul = document.querySelector("ul");
+const popUp = document.querySelector(".popUp");
+const popUpMessage = document.querySelector(".popUpMessage");
 
 let initTime = 0;
 let startGameTimer;
 let movementCount = 0;
 let TILENUMBER = 9;
+let movemoentArray = [];
+let swapCount;
 
 var addPlayBtnEvent = playBtn.addEventListener("click", startGame);
 var addShuffleBtnEvent = shuffleBtn.addEventListener("click", shuffle);
-var addPhotoChangeBtnEvent = photoChangeBtn.addEventListener(
-  "click",
-  changePhoto
-);
+var addUndoBtnEvent = undoBtn.addEventListener("click", undoMovement);
+var addReplayBtnEvent = replayBtn.addEventListener("click", replayGame);
 
 function startGame() {
   removePlayBtnEvent();
   setGameTimer();
-  shuffle();
+  createTileLists();
+  //shuffle();
 }
 
 function removePlayBtnEvent() {
@@ -45,36 +50,84 @@ function setGameTimer() {
   // clearInterval(setGameTimer);
 }
 
+function createTileLists() {
+  let c = 1;
+  for (let i = 1; i <= TILENUMBER; i++) {
+    if (i <= TILENUMBER / 3) {
+      let r = 1;
+      ul.innerHTML += `<li id="cell${r}${c}" class="tile${i}" onClick="clickTile(${r},${c});"></li>`;
+    } else if (i > 3 && i <= 6) {
+      r = 2;
+      ul.innerHTML += `<li id="cell${r}${
+        c - 3
+      }" class="tile${i}" onClick="clickTile(${r},${c - 3});"></li>`;
+    } else if (i > 6 && i <= 9) {
+      r = 3;
+      ul.innerHTML += `<li id="cell${r}${
+        c - 6
+      }" class="tile${i}" onClick="clickTile(${r},${c - 6});"></li>`;
+    }
+    c++;
+  }
+}
+
+function displayMoveMentNumber(numberChange) {
+  numberChange;
+  swapCount = movementCount - TILENUMBER;
+  movement.innerHTML = `Movement : ${swapCount}`;
+}
+///////////////////////////////////////////////////////
+// <li id="cell11" class="tile1" onClick="clickTile(1,1);"></li>
+// <li id="cell12" class="tile2" onClick="clickTile(1,2);"></li>
+// <li id="cell13" class="tile3" onClick="clickTile(1,3);"></li>
+// <li id="cell21" class="tile4" onClick="clickTile(2,1);"></li>
+// <li id="cell22" class="tile5" onClick="clickTile(2,2);"></li>
+// <li id="cell23" class="tile6" onClick="clickTile(2,3);"></li>
+// <li id="cell31" class="tile7" onClick="clickTile(3,1);"></li>
+// <li id="cell32" class="tile8" onClick="clickTile(3,2);"></li>
+// <li id="cell33" class="tile9" onClick="clickTile(3,3);"></li>
+////////////////////////////////////////////////////////
 function shuffle() {
   movementCount = 0;
-  //Use nested loops to access each cell of the 3x3 grid
   for (let row = 1; row <= 3; row++) {
-    //For each row of the 3x3 grid
     for (let column = 1; column <= 3; column++) {
-      //For each column in this row
-
-      let row2 = Math.floor(Math.random() * 3 + 1); //Pick a random row from 1 to 3
-      let column2 = Math.floor(Math.random() * 3 + 1); //Pick a random column from 1 to 3
-
-      swapTiles("cell" + row + column, "cell" + row2 + column2); //Swap the look & feel of both cells
+      let row2 = Math.floor(Math.random() * 3 + 1);
+      let column2 = Math.floor(Math.random() * 3 + 1);
+      swapTiles("cell" + row + column, "cell" + row2 + column2);
     }
   }
 }
 
-function swapTiles(cell1, cell2) {
+function undoMovement() {
+  if (swapCount > 0) {
+    let cell2 = movemoentArray.slice(-1);
+    movemoentArray.pop();
+    let cell1 = movemoentArray.slice(-1);
+    movemoentArray.pop();
+    swapTiles(cell2, cell1, "undo");
+  }
+}
+
+function swapTiles(cell1, cell2, doOrUndo) {
+  document.getElementById(cell1);
   let temp = document.getElementById(cell1).className;
   document.getElementById(cell1).className = document.getElementById(
     cell2
   ).className;
   document.getElementById(cell2).className = temp;
-  ++movementCount;
-  let swapCount = movementCount - TILENUMBER;
-  movement.innerHTML = `Movement : ${swapCount}`;
+  if (!doOrUndo) {
+    displayMoveMentNumber(++movementCount);
+    movemoentArray.push(cell2);
+    movemoentArray.push(cell1);
+  } else if (doOrUndo === "undo") {
+    displayMoveMentNumber(--movementCount);
+  }
+  checkSolution();
 }
 
 function clickTile(row, column) {
-  let cell = document.getElementById("cell" + row + column);
-  let tile = cell.className;
+  var cell = document.getElementById("cell" + row + column);
+  var tile = cell.className;
   if (tile != "tile9") {
     //Checking if white tile on the right
     if (column < 3) {
@@ -120,35 +173,26 @@ function clickTile(row, column) {
 }
 
 function checkSolution() {
-  if (tileMap.empty.position !== 9) return false;
-
-  for (var key in tileMap) {
-    if (key != 1 && key != "empty") {
-      if (tileMap[key].position < tileMap[key - 1].position) return false;
+  checkTileNum = 0;
+  for (let i = 1; i <= TILENUMBER; i++) {
+    let tileClassName = document.querySelectorAll("li")[`${i - 1}`].className;
+    let tileNumber = `tile${i}`;
+    console.log(tileNumber);
+    if (tileClassName === tileNumber) {
+      checkTileNum++;
+      console.log(checkTileNum);
+      if (checkTileNum === 9) {
+        displayPopUp("Good Job ! 👍");
+      }
     }
   }
-
-  // Clear history if solved
-  history = [];
-  return true;
 }
 
-function changePhoto() {
-  originalPhoto.alt === "game1" ? changeTiles(2) : changeTiles(1);
+function displayPopUp(message) {
+  popUp.style.visibility = "visible";
+  popUpMessage.innerHTML = `${message}`;
 }
 
-function changeTiles(photoNum) {
-  for (let i = 1; i < TILENUMBER + 1; i++) {
-    const tiles = document.querySelector(`.tile${i}`);
-    tiles.style.backgroundImage =
-      "url('" + "imgs/game" + photoNum + ".png" + "')";
-    if (i === 9) {
-      tiles.style.background = "white";
-    }
-  }
-  originalPhoto.src = `imgs/game${photoNum}.png`;
-  originalPhoto.alt = `game${photoNum}`;
-}
+function replayGame() {}
 
-// 게임 끝남
 // 난이도 바꾸기
