4ab0fcf1f5446fbceec88a8bfc700ca66a3e16f2
HanYoung23 <67942048+HanYoung23@users.noreply.github.com>
2020-10-10 16:44:19 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-const threeBtn = document.querySelector(".threeBythree");
 const playBtn = document.querySelector(".playBtn");
 const shuffleBtn = document.querySelector(".shuffleBtn");
 const photoChangeBtn = document.querySelector(".photoChangeBtn");
 const undoBtn = document.querySelector(".undoBtn");
 const replayBtn = document.querySelector(".popUpReplayBtn");
-const threeByThreeBtn = document.querySelector(".threeByThreeBtn");
-const fourByFourBtn = document.querySelector(".fourByFourBtn");
-const fiveByFiveBtn = document.querySelector(".fiveByFiveBtn");
+const threeDimensionBtn = document.querySelector(".threeByThree");
+const fourDimensionBtn = document.querySelector(".fourByFour");
+const fiveDimensionBtn = document.querySelector(".fiveByFive");
 
 const timer = document.querySelector(".timer");
 const movement = document.querySelector(".movement");
@@ -21,8 +20,8 @@ let movemoentArray = [];
 let swapCount;
 
 // 타일 생성 변수
-let DIMENSION = 3;
-let TILE_NUMBER = Math.sqrt(DIMENSION);
+let DIMENSION;
+let TILE_NUMBER;
 let TILE_BACKGROUND_SIZE = 360;
 let TILE_WIDTH = TILE_BACKGROUND_SIZE / DIMENSION;
 let TILE_HEIGHTH = TILE_BACKGROUND_SIZE / DIMENSION;
@@ -31,28 +30,40 @@ var addPlayBtnEvent = playBtn.addEventListener("click", startGame);
 var addShuffleBtnEvent = shuffleBtn.addEventListener("click", shuffle);
 var addUndoBtnEvent = undoBtn.addEventListener("click", undoMovement);
 var addReplayBtnEvent = replayBtn.addEventListener("click", replayGame);
-var addThreeByThreeBtnEvent = threeByThreeBtn.addEventListener(
+var addThreeByThreeBtnEvent = threeDimensionBtn.addEventListener(
   "click",
-  changeDimensionToThree
+  changeDimensionThree
 );
-var addFourByFourBtnEvent = fourByFourBtn.addEventListener(
+var addFourByFourBtnEvent = fourDimensionBtn.addEventListener(
   "click",
-  changeDimensionToFour
+  changeDimensionFour
 );
-var addFiveByFiveBtnEvent = fiveByFiveBtn.addEventListener(
+var addFiveByFiveBtnEvent = fiveDimensionBtn.addEventListener(
   "click",
-  changeDimensionToFive
+  changeDimensionFive
 );
 
-function startGame(fromReplay) {
-  if (fromReplay === "replay") {
-    return;
+BtnOnAndOff("shuffleBtnOff");
+
+function changeDimensionThree() {
+  createTileLists(3);
+}
+function changeDimensionFour() {
+  createTileLists(4);
+}
+function changeDimensionFive() {
+  createTileLists(5);
+}
+
+function startGame() {
+  if (DIMENSION === undefined) {
+    alert("Dimension을 선택하세요");
   } else {
-    createTileLists();
+    BtnOnAndOff("playBtnOff");
+    setGameTimer(0);
+    shuffle();
+    BtnOnAndOff("shuffleBtnOn");
   }
-  BtnOnAndOff("playBtnOff");
-  setGameTimer();
-  shuffle();
 }
 
 function setGameTimer(resetTime) {
@@ -67,11 +78,9 @@ function setTimerFunc() {
   let seconds = initTime % 60;
   let minutes = Math.floor((initTime / 60) % 60);
   let hours = Math.floor((initTime / 3600) % 60);
-
   hour = hours < 10 ? "0" + hours : hours;
   min = minutes < 10 ? "0" + minutes : minutes;
   sec = seconds < 10 ? "0" + seconds : seconds;
-
   timer.innerHTML = `Time - ${hour}:${min}:${sec}`;
 }
 
@@ -82,48 +91,48 @@ function displayMoveMentNumber(numberChange) {
 }
 
 // ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
-
-function createTileLists() {
+function createTileLists(dimension) {
+  ul.innerHTML = "";
+  DIMENSION = dimension;
+  TILE_NUMBER = DIMENSION * DIMENSION;
+  TILE_WIDTH = TILE_BACKGROUND_SIZE / DIMENSION;
+  TILE_HEIGHTH = TILE_BACKGROUND_SIZE / DIMENSION;
+  let x = TILE_WIDTH;
+  let y = TILE_HEIGHTH;
   let row;
-  let column = 1;
+  let col = 1;
+  let n = 1;
   for (let i = 1; i <= TILE_NUMBER; i++) {
-    if (i <= DIMENSION) {
-      row = 1;
-      ul.innerHTML += `<li id="cell${row}${column}" class="tile${i}" onClick="clickTile(${row},${column});"></li>`;
-    } else if (i > 3 && i <= 6) {
-      row = 2;
-      ul.innerHTML += `<li id="cell${row}${
-        column - 3
-      }" class="tile${i}" onClick="clickTile(${row},${column - 3});"></li>`;
-    } else if (i > 6 && i <= 9) {
-      row = 3;
-      ul.innerHTML += `<li id="cell${row}${
-        column - 6
-      }" class="tile${i}" onClick="clickTile(${row},${column - 6});"></li>`;
-    }
-    c++;
+    ul.innerHTML += `<li class="tile${i}"></li>`;
   }
+  for (row = 1; row <= DIMENSION; row++) {
+    for (col = 1; col <= DIMENSION; col++) {
+      const tiles = document.querySelector(`.tile${n}`);
+      tiles.setAttribute("id", `cell${row}${col}`);
+      tiles.setAttribute("onclick", `clickTile(${row},${col})`);
+      tiles.style.left = `${x}` * (`${col}` - 1) + "px";
+      tiles.style.top = `${y}` * (`${row}` - 1) + "px";
+      tiles.style.width = `${TILE_WIDTH}px`;
+      tiles.style.height = `${TILE_HEIGHTH}px`;
+      tiles.style.backgroundPosition =
+        -`${x}` * (`${col}` - 1) + "px " + -`${y}` * (`${row}` - 1) + "px";
+      // tiles.style.backgroundImage = "url(" + "imgs/game2.png" + ")";
+      n++;
+      if (n === TILE_NUMBER) {
+        tiles.style.backgroundPosition = "null";
+      }
+    }
+  }
+  BtnOnAndOff("playBtnOn");
 }
-//https://codepen.io/bongam/pen/yrwYWJ
 
-///////////////////////////////////////////////////////
-// <li id="cell11" class="tile1" onClick="clickTile(1,1);"></li>
-// <li id="cell12" class="tile2" onClick="clickTile(1,2);"></li>
-// <li id="cell13" class="tile3" onClick="clickTile(1,3);"></li>
-// <li id="cell21" class="tile4" onClick="clickTile(2,1);"></li>
-// <li id="cell22" class="tile5" onClick="clickTile(2,2);"></li>
-// <li id="cell23" class="tile6" onClick="clickTile(2,3);"></li>
-// <li id="cell31" class="tile7" onClick="clickTile(3,1);"></li>
-// <li id="cell32" class="tile8" onClick="clickTile(3,2);"></li>
-// <li id="cell33" class="tile9" onClick="clickTile(3,3);"></li>
-////////////////////////////////////////////////////////
 function shuffle() {
   movementCount = 0;
-  for (let row = 1; row <= 3; row++) {
+  for (let row1 = 1; row1 <= 3; row1++) {
     for (let column = 1; column <= 3; column++) {
       let row2 = Math.floor(Math.random() * 3 + 1);
       let column2 = Math.floor(Math.random() * 3 + 1);
-      swapTiles("cell" + row + column, "cell" + row2 + column2);
+      swapTiles("cell" + row1 + column, "cell" + row2 + column2);
     }
   }
 }
@@ -135,6 +144,18 @@ function swapTiles(cell1, cell2, doOrUndo) {
     cell2
   ).className;
   document.getElementById(cell2).className = temp;
+
+  document.getElementById(cell1);
+  let tileBg = document.getElementById(cell1).style.backgroundPosition;
+  document.getElementById(
+    cell1
+  ).style.backgroundPosition = document.getElementById(
+    cell2
+  ).style.backgroundPosition;
+  document.getElementById(cell2).style.backgroundPosition = tileBg;
+
+  // document.querySelector(`.tile${TILE_NUMBER}`).style.background = "white";
+  //undo
   if (!doOrUndo) {
     displayMoveMentNumber(++movementCount);
     movemoentArray.push(cell2);
@@ -146,47 +167,49 @@ function swapTiles(cell1, cell2, doOrUndo) {
 }
 
 function clickTile(row, column) {
-  var cell = document.getElementById("cell" + row + column);
-  var tile = cell.className;
-  if (tile != "tile9") {
-    //Checking if white tile on the right
-    if (column < 3) {
-      if (
-        document.getElementById("cell" + row + (column + 1)).className ==
-        "tile9"
-      ) {
-        swapTiles("cell" + row + column, "cell" + row + (column + 1));
-        return;
+  if (initTime > 0) {
+    var cell = document.getElementById("cell" + row + column);
+    var tile = cell.className;
+    if (tile != "tile9") {
+      //Checking if white tile on the right
+      if (column < 3) {
+        if (
+          document.getElementById("cell" + row + (column + 1)).className ==
+          "tile9"
+        ) {
+          swapTiles("cell" + row + column, "cell" + row + (column + 1));
+          return;
+        }
       }
-    }
-    //Checking if white tile on the left
-    if (column > 1) {
-      if (
-        document.getElementById("cell" + row + (column - 1)).className ==
-        "tile9"
-      ) {
-        swapTiles("cell" + row + column, "cell" + row + (column - 1));
-        return;
+      //Checking if white tile on the left
+      if (column > 1) {
+        if (
+          document.getElementById("cell" + row + (column - 1)).className ==
+          "tile9"
+        ) {
+          swapTiles("cell" + row + column, "cell" + row + (column - 1));
+          return;
+        }
       }
-    }
-    //Checking if white tile is above
-    if (row > 1) {
-      if (
-        document.getElementById("cell" + (row - 1) + column).className ==
-        "tile9"
-      ) {
-        swapTiles("cell" + row + column, "cell" + (row - 1) + column);
-        return;
+      //Checking if white tile is above
+      if (row > 1) {
+        if (
+          document.getElementById("cell" + (row - 1) + column).className ==
+          "tile9"
+        ) {
+          swapTiles("cell" + row + column, "cell" + (row - 1) + column);
+          return;
+        }
       }
-    }
-    //Checking if white tile is below
-    if (row < 3) {
-      if (
-        document.getElementById("cell" + (row + 1) + column).className ==
-        "tile9"
-      ) {
-        swapTiles("cell" + row + column, "cell" + (row + 1) + column);
-        return;
+      //Checking if white tile is below
+      if (row < 3) {
+        if (
+          document.getElementById("cell" + (row + 1) + column).className ==
+          "tile9"
+        ) {
+          swapTiles("cell" + row + column, "cell" + (row + 1) + column);
+          return;
+        }
       }
     }
   }
@@ -205,16 +228,16 @@ function undoMovement() {
 }
 
 function checkSolution() {
-  checkTileNum = 0;
-  for (let i = 1; i <= TILE_NUMBER; i++) {
-    let tileClassName = document.querySelectorAll("li")[`${i - 1}`].className;
-    let tileNumber = `tile${i}`;
-    console.log(tileNumber);
-    if (tileClassName === tileNumber) {
-      checkTileNum++;
-      console.log(checkTileNum);
-      if (checkTileNum === 9) {
-        displayPopUp("Good Job ! 👍");
+  if (swapCount !== 0) {
+    checkTileNum = 0;
+    for (let i = 1; i <= TILE_NUMBER; i++) {
+      let tileClassName = document.querySelectorAll("li")[`${i - 1}`].className;
+      let tileNumber = `tile${i}`;
+      if (tileClassName === tileNumber) {
+        checkTileNum++;
+        if (checkTileNum === TILE_NUMBER) {
+          displayPopUp("Good Job ! 👍");
+        }
       }
     }
   }
@@ -228,8 +251,7 @@ function displayPopUp(message) {
 }
 
 function replayGame() {
-  startGame("replay");
-  setGameTimer(0);
+  startGame();
   popUp.style.visibility = "hidden";
   BtnOnAndOff("allBtnOn");
 }
@@ -238,6 +260,12 @@ function BtnOnAndOff(onAndOff) {
   switch (onAndOff) {
     case "playBtnOff":
       playBtn.removeEventListener("click", startGame);
+      break;
+    case "shuffleBtnOn":
+      shuffleBtn.addEventListener("click", shuffle);
+      break;
+    case "shuffleBtnOff":
+      shuffleBtn.removeEventListener("click", shuffle);
       break;
     case "allBtnOn":
       shuffleBtn.addEventListener("click", shuffle);
@@ -252,29 +280,5 @@ function BtnOnAndOff(onAndOff) {
   }
 }
 
-function changeDimensionToThree(dimension) {
-  DIMENSION = dimension;
-  TILE_NUMBER = Math.sqrt(dimension);
-}
-
-function changeDimensionToFour() {
-  DIMENSION = 16;
-}
-
-function changeDimensionToFive() {
-  DIMENSION = 25;
-}
-
-// var addThreeByThreeBtnEvent = threeByThreeBtn.addEventListener(
-//   "click",
-//   changeDimensionToThree
-// );
-// var addFourByFourBtnEvent = fourByFourBtn.addEventListener(
-//   "click",
-//   changeDimensionToFour
-// );
-// var addFiveByFiveBtnEvent = fiveByFiveBtn.addEventListener(
-//   "click",
-//   changeDimensionToFive
-// );
 // 난이도 바꾸기
+//https://codepen.io/bongam/pen/yrwYWJ
