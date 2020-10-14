8d42a7acd71dadaa427ca2d628afa2f9643f55ea
HanYoung23 <67942048+HanYoung23@users.noreply.github.com>
2020-10-14 17:52:59 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -6,9 +6,7 @@ const playBtn = document.querySelector(".playBtn");
 const shuffleBtn = document.querySelector(".shuffleBtn");
 const undoBtn = document.querySelector(".undoBtn");
 const replayBtn = document.querySelector(".popUpReplayBtn");
-const threeDimensionBtn = document.querySelector(".threeByThree");
-const fourDimensionBtn = document.querySelector(".fourByFour");
-const fiveDimensionBtn = document.querySelector(".fiveByFive");
+const settingBtns = document.querySelector(".gameSettingBtns");
 
 const timer = document.querySelector(".timer");
 const movement = document.querySelector(".movement");
@@ -20,72 +18,61 @@ const originalPhoto = document.querySelector(".originalPhoto");
 let initTime = 0;
 let startGameTimer;
 let movementCount = 0;
-let movemoentArray = [];
+let movementArray = [];
 let swapCount;
-let dimensionOnOff;
-let clickTileOnOff = "on";
 
 let DIMENSION = 0;
 let TILE_NUMBER;
 let TILE_BACKGROUND_SIZE = 360;
-let TILE_WIDTH = TILE_BACKGROUND_SIZE / DIMENSION;
-let TILE_HEIGHTH = TILE_BACKGROUND_SIZE / DIMENSION;
 
 replayBtn.addEventListener("click", replayGame);
 refreshBtn.addEventListener("click", refreshGame);
 photoBtn.addEventListener("click", changePhoto);
 playBtn.addEventListener("click", startGame);
-threeDimensionBtn.addEventListener("click", () => {
-  changeDimension(3);
-});
-fourDimensionBtn.addEventListener("click", () => {
-  changeDimension(4);
-});
-fiveDimensionBtn.addEventListener("click", () => {
-  changeDimension(5);
-});
+//settingBtns.addEventListener("click", setDimension);
 
-function changeDimension(dimensionOrSwitch) {
-  if (dimensionOrSwitch === "on") {
-    dimensionOnOff = "on";
-    return;
-  } else if (dimensionOrSwitch === "off") {
-    dimensionOnOff = "off";
-    return;
-  } else if (dimensionOnOff !== "off" && Number.isInteger(dimensionOrSwitch)) {
-    createTileLists(dimensionOrSwitch);
+var setDimension = (event) => {
+  const target = parseInt(event.target.id.charAt(0));
+  if (Number.isInteger(target)) {
+    console.log("asdf");
+    createTileLists(target);
   }
-}
+};
+settingBtns.addEventListener("click", setDimension);
 
-function refreshGame() {
-  clearInterval(startGameTimer);
-  replayGame();
-}
-
-function replayGame() {
-  timer.innerHTML = `Time - 00:00:00`;
-  movement.innerHTML = `Movement : 0`;
-  DIMENSION = 0;
+function createTileLists(dimension) {
+  ul.addEventListener("click", clickTile);
   ul.innerHTML = "";
-  popUp.style.visibility = "hidden";
-  changeDimension("on");
-  controlFunctionBtns("playBtn", "add", startGame);
-}
-
-function startGame() {
-  if (DIMENSION === 0) {
-    alert("3x3 / 4x4 / 5x5 중 하나를 선택하세요");
-  } else {
-    setGameTimer(0);
-    controlFunctionBtns("playBtn", "remove", startGame);
-    controlFunctionBtns("shuffleBtn", "add", shuffle);
-    controlFunctionBtns("undoBtn", "add", undoMovement);
-    controlClickTile("on");
-    changeDimension("off");
-    if (checkSolution) {
-      shuffle();
+  DIMENSION = dimension;
+  TILE_NUMBER = dimension * dimension;
+  let TILE_WIDTH = TILE_BACKGROUND_SIZE / dimension;
+  let TILE_HEIGHTH = TILE_BACKGROUND_SIZE / dimension;
+  let x = TILE_WIDTH;
+  let y = TILE_HEIGHTH;
+  let tileNum = 1;
+  for (let i = 1; i <= TILE_NUMBER; i++) {
+    ul.innerHTML += `<li class="tile${i}"></li>`;
+  }
+  for (let row = 1; row <= DIMENSION; row++) {
+    for (let col = 1; col <= DIMENSION; col++) {
+      let tiles = document.querySelector(`.tile${tileNum}`);
+      tiles.setAttribute("id", `cell${row}${col}`);
+      tiles.style.left = `${x}` * (`${col}` - 1) + "px";
+      tiles.style.top = `${y}` * (`${row}` - 1) + "px";
+      tiles.style.width = `${TILE_WIDTH}px`;
+      tiles.style.height = `${TILE_HEIGHTH}px`;
+      tiles.style.backgroundPosition =
+        -`${x}` * (`${col}` - 1) + "px " + -`${y}` * (`${row}` - 1) + "px";
+      tiles.style.backgroundImage = `url(${originalPhoto.src})`;
+      if (tileNum === TILE_NUMBER) {
+        tiles.style.backgroundImage = `url(imgs/emptyBackground.png)`;
+      }
+      tileNum++;
     }
   }
+  playBtn.addEventListener("click", startGame);
+  shuffleBtn.removeEventListener("click", shuffle);
+  ul.removeEventListener("click", clickTile);
 }
 
 function setGameTimer(resetTime) {
@@ -112,6 +99,68 @@ function displayMoveMentNumber(numberChange) {
   movement.innerHTML = `Movement : ${swapCount}`;
 }
 
+function replayGame() {
+  timer.innerHTML = `Time - 00:00:00`;
+  movement.innerHTML = `Movement : 0`;
+  DIMENSION = 0;
+  ul.innerHTML = "";
+  popUp.style.visibility = "hidden";
+  settingBtns.addEventListener("click", setDimension);
+  playBtn.addEventListener("click", startGame);
+}
+
+function display(message) {
+  clearInterval(startGameTimer);
+  popUp.style.visibility = "visible";
+  popUpMessage.innerHTML = `${message}`;
+  shuffleBtn.removeEventListener("click", shuffle);
+  undoBtn.removeEventListener("click", undoMovement);
+  settingBtns.removeEventListener("click", setDimension);
+  ul.removeEventListener("click", clickTile);
+}
+
+function startGame() {
+  if (DIMENSION === 0) {
+    alert("3x3 / 4x4 / 5x5 중 하나를 선택하세요");
+  } else {
+    setGameTimer(0);
+    playBtn.removeEventListener("click", startGame);
+    shuffleBtn.addEventListener("click", shuffle);
+    undoBtn.addEventListener("click", undoMovement);
+    ul.addEventListener("click", clickTile);
+    settingBtns.removeEventListener("click", setDimension);
+    if (checkSolution) {
+      shuffle();
+    }
+  }
+}
+
+function shuffle() {
+  movementCount = 0;
+  for (let row1 = 1; row1 <= DIMENSION; row1++) {
+    for (let column = 1; column <= DIMENSION; column++) {
+      let row2 = Math.floor(Math.random() * DIMENSION + 1);
+      let column2 = Math.floor(Math.random() * DIMENSION + 1);
+      swapTiles("cell" + row1 + column, "cell" + row2 + column2);
+    }
+  }
+}
+
+function undoMovement() {
+  if (swapCount > 0) {
+    let cell2 = movementArray.slice(-1);
+    movementArray.pop();
+    let cell1 = movementArray.slice(-1);
+    movementArray.pop();
+    swapTiles(cell2, cell1, "undo");
+  }
+}
+
+function refreshGame() {
+  clearInterval(startGameTimer);
+  replayGame();
+}
+
 function changePhoto() {
   if (originalPhoto.alt === "game1") {
     originalPhoto.src = "imgs/game2.png";
@@ -127,58 +176,6 @@ function changePhoto() {
     for (let i = 1; i < TILE_NUMBER; i++) {
       const tiles = document.querySelector(`.tile${i}`);
       tiles.style.backgroundImage = `url(${originalPhoto.src})`;
-    }
-  }
-}
-
-function createTileLists(dimension) {
-  ul.innerHTML = "";
-  DIMENSION = dimension;
-  TILE_NUMBER = DIMENSION * DIMENSION;
-  TILE_WIDTH = TILE_BACKGROUND_SIZE / DIMENSION;
-  TILE_HEIGHTH = TILE_BACKGROUND_SIZE / DIMENSION;
-  let x = TILE_WIDTH;
-  let y = TILE_HEIGHTH;
-  let row;
-  let col = 1;
-  let n = 1;
-  for (let i = 1; i <= TILE_NUMBER; i++) {
-    ul.innerHTML += `<li class="tile${i}"></li>`;
-  }
-  for (row = 1; row <= DIMENSION; row++) {
-    for (col = 1; col <= DIMENSION; col++) {
-      const tiles = document.querySelector(`.tile${n}`);
-      tiles.setAttribute("id", `cell${row}${col}`);
-      tiles.setAttribute("onclick", `clickTile(${row},${col})`);
-      tiles.style.left = `${x}` * (`${col}` - 1) + "px";
-      tiles.style.top = `${y}` * (`${row}` - 1) + "px";
-      tiles.style.width = `${TILE_WIDTH}px`;
-      tiles.style.height = `${TILE_HEIGHTH}px`;
-      tiles.style.backgroundPosition =
-        -`${x}` * (`${col}` - 1) + "px " + -`${y}` * (`${row}` - 1) + "px";
-      tiles.style.backgroundImage = `url(${originalPhoto.src})`;
-      if (n === TILE_NUMBER) {
-        tiles.style.backgroundImage = "url(" + "imgs/emptyBackground.png" + ")";
-      }
-      n++;
-    }
-  }
-  controlFunctionBtns("playBtn", "add", startGame);
-  controlFunctionBtns("shuffleBtn", "remove", shuffle);
-  controlClickTile("off");
-}
-
-function controlClickTile(onOff) {
-  clickTileOnOff = onOff;
-}
-
-function shuffle() {
-  movementCount = 0;
-  for (let row1 = 1; row1 <= DIMENSION; row1++) {
-    for (let column = 1; column <= DIMENSION; column++) {
-      let row2 = Math.floor(Math.random() * DIMENSION + 1);
-      let column2 = Math.floor(Math.random() * DIMENSION + 1);
-      swapTiles("cell" + row1 + column, "cell" + row2 + column2);
     }
   }
 }
@@ -201,73 +198,53 @@ function swapTiles(cell1, cell2, doOrUndo) {
 
   if (!doOrUndo) {
     displayMoveMentNumber(++movementCount);
-    movemoentArray.push(cell2);
-    movemoentArray.push(cell1);
+    movementArray.push(cell2);
+    movementArray.push(cell1);
   } else if (doOrUndo === "undo") {
     displayMoveMentNumber(--movementCount);
   }
   checkSolution();
 }
 
-function clickTile(row, column) {
-  const lastTile = document.querySelector(`.tile${TILE_NUMBER}`).className;
-  if (clickTileOnOff === "on") {
-    var cell = document.getElementById("cell" + row + column);
-    var tile = cell.className;
-    if (tile != lastTile) {
-      //Checking if white tile on the right
-      if (column < DIMENSION) {
-        if (
-          document.getElementById("cell" + row + (column + 1)).className ==
-          lastTile
-        ) {
-          swapTiles("cell" + row + column, "cell" + row + (column + 1));
-          return;
-        }
+var clickTile = (event) => {
+  const row = parseInt(event.target.id.charAt(4));
+  const column = parseInt(event.target.id.charAt(5));
+  const lastTile = document.querySelector(`.tile${TILE_NUMBER}`);
+  const rightTile = document.querySelector("#cell" + row + (column + 1));
+  const leftTile = document.querySelector("#cell" + row + (column - 1));
+  const aboveTile = document.querySelector("#cell" + (row - 1) + column);
+  const belowTile = document.querySelector("#cell" + (row + 1) + column);
+  if (event.target.className != lastTile) {
+    //Checking if white tile on the right
+    if (column < DIMENSION) {
+      if (rightTile.className === lastTile.className) {
+        swapTiles("cell" + row + column, "cell" + row + (column + 1));
+        return;
       }
-      //Checking if white tile on the left
-      if (column > 1) {
-        if (
-          document.getElementById("cell" + row + (column - 1)).className ==
-          lastTile
-        ) {
-          swapTiles("cell" + row + column, "cell" + row + (column - 1));
-          return;
-        }
+    }
+    //Checking if white tile on the left
+    if (column > 1) {
+      if (leftTile.className === lastTile.className) {
+        swapTiles("cell" + row + column, "cell" + row + (column - 1));
+        return;
       }
-      //Checking if white tile is above
-      if (row > 1) {
-        if (
-          document.getElementById("cell" + (row - 1) + column).className ==
-          lastTile
-        ) {
-          swapTiles("cell" + row + column, "cell" + (row - 1) + column);
-          return;
-        }
+    }
+    //Checking if white tile is above
+    if (row > 1) {
+      if (aboveTile.className === lastTile.className) {
+        swapTiles("cell" + row + column, "cell" + (row - 1) + column);
+        return;
       }
-      //Checking if white tile is below
-      if (row < DIMENSION) {
-        if (
-          document.getElementById("cell" + (row + 1) + column).className ==
-          lastTile
-        ) {
-          swapTiles("cell" + row + column, "cell" + (row + 1) + column);
-          return;
-        }
+    }
+    //Checking if white tile is below
+    if (row < DIMENSION) {
+      if (belowTile.className === lastTile.className) {
+        swapTiles("cell" + row + column, "cell" + (row + 1) + column);
+        return;
       }
     }
   }
-}
-
-function undoMovement() {
-  if (swapCount > 0) {
-    let cell2 = movemoentArray.slice(-1);
-    movemoentArray.pop();
-    let cell1 = movemoentArray.slice(-1);
-    movemoentArray.pop();
-    swapTiles(cell2, cell1, "undo");
-  }
-}
+};
 
 function checkSolution() {
   if (swapCount !== 0) {
@@ -278,29 +255,9 @@ function checkSolution() {
       if (tileClassName === tileNumber) {
         checkTileNum++;
         if (checkTileNum === TILE_NUMBER) {
-          displayPopUp("Good Job ! 👍");
+          display("Good Job ! 👍");
         }
       }
     }
-  }
-}
-
-function displayPopUp(message) {
-  popUp.style.visibility = "visible";
-  popUpMessage.innerHTML = `${message}`;
-  clearInterval(startGameTimer);
-  controlFunctionBtns("shuffleBtn", "remove", shuffle);
-  controlFunctionBtns("undoBtn", "remove", undoMovement);
-  changeDimension("off");
-  controlClickTile("off");
-}
-
-function controlFunctionBtns(btnName, removeOrAdd, functionName) {
-  const btnSelector = document.querySelector(`.${btnName}`);
-  const btnFunction = functionName;
-  if (removeOrAdd === "remove") {
-    btnSelector.removeEventListener("click", btnFunction);
-  } else if (removeOrAdd === "add") {
-    btnSelector.addEventListener("click", btnFunction);
   }
 }
